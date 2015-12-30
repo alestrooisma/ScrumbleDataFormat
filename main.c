31908e797f6e38352c8f312b31484dbe55243b19
Ale Strooisma <ale@strooisma>
2015-12-30 15:46:26 +0100

    Added some test cases

--- main.c
+++ main.c
@@ -6,23 +6,71 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <dirent.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #include "SdfParser.h"
 
-/*
- * Main function to test the SDF-parser
- */
-int main(int argc, char** argv) {
-	SdfNode* root = sdf_parse_file("sampledata/asteroidtypes.sdf");
-	if (root != NULL) {
-		printf("parsing successful!\n\n");
-		sdf_draw_tree(stdout, root);
-		sdf_free_tree(root);
-		return (EXIT_SUCCESS);
+#define MAX 100
+
+void processDirectory(char* path, char* prefix) {
+	char* list[MAX];
+	int i;
+	for (i = 0; i < MAX; i++) {
+		list[i] = NULL;
+	}
+	
+	int prefix_length = strlen(prefix);
+	DIR *dir;
+	struct dirent *entry;
+	if ((dir = opendir(path)) != NULL) {
+		while ((entry = readdir(dir)) != NULL) {
+			char* fname = entry->d_name;
+			int fname_length = strlen(fname);
+			if (strncmp(fname, prefix, prefix_length) == 0) {
+				int number = atoi(&fname[prefix_length]);
+				if (number < MAX) {
+					list[number] = (char*) malloc(sizeof(char) * (strlen(path) + fname_length + 1));
+					strcpy(list[number], path);
+					strcat(list[number], fname);
+				}
+			}
+		}
+		closedir(dir);
 	} else {
-		printf("parsing failed...\n");
-		return (EXIT_FAILURE);
+		/* could not open directory */
+		perror("");
+	}
+	
+	for (i = 0; i < MAX; i++) {
+		if (list[i]) {
+			printf("Parsing %s\n", list[i]);
+			SdfNode* root = sdf_parse_file(list[i]);
+			sdf_draw_tree(stdout, root);
+			sdf_free_tree(root);
+			if (root) {
+				printf("Success!\n\n");
+			}
+			free(list[i]);
+		}
 	}
 }
 
+/**
+ * Main function to test the SDF-parser
+ */
+int main(int argc, char** argv) {
+	processDirectory("sampledata/correct/", "correct");
+	//	SdfNode* root = sdf_parse_file("sampledata/asteroidtypes.sdf");
+	//	if (root != NULL) {
+	//		printf("parsing successful!\n\n");
+	//		sdf_draw_tree(stdout, root);
+	//		sdf_free_tree(root);
+	//		return (EXIT_SUCCESS);
+	//	} else {
+	//		printf("parsing failed...\n");
+	//		return (EXIT_FAILURE);
+	//	}
+}
